643a0b8f15ea2a54734c9920c568eb9ee8a90f73
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2018-03-22 00:14:09 +0100

    improve loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,6 +1,16 @@
 // @flow
 import React from 'react'
+import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
 
-const Loading = () => <h1>Loading...</h1>
+const Loading = () => (
+  <div>
+    <Segment>
+      <Dimmer active inverted>
+        <Loader inverted>Loading</Loader>
+      </Dimmer>
+      <Image src='/assets/images/wireframe/short-paragraph.png' />
+    </Segment>
+  </div>
+)
 
 export default Loading
