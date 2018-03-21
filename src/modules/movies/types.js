081722093a2d6f66a288b0c98ae9f690e6223a01
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2018-03-21 23:29:39 +0100

    complete movie types

--- src/modules/movies/types.js
+++ src/modules/movies/types.js
@@ -1,8 +1,19 @@
 // @flow
 export type Movie = {
+  vote_count: number,
   id: number,
+  video: boolean,
+  vote_average: number,
   title: string,
-  overview: string
+  popularity: number,
+  poster_path: string,
+  original_language: string,
+  original_title: string,
+  genre_ids: Array<number>,
+  backdrop_path: string,
+  adult: boolean,
+  overview: string,
+  release_date: string
 }
 
 export type Movies = Array<Movie>
