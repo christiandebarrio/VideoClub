76df2b112651ec053510ea4321ce07a06f82af44
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2018-03-21 23:42:26 +0100

    insert back home link in error

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,6 +1,15 @@
 // @flow
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Error404 = () => <h1>Error 404 - Page not found</h1>
+const Error404 = () => (
+  <div>
+    <span>404</span>
+    <p>PAGE NOT FOUND</p>
+    <Link to='/' className='back'>
+      Back To Home
+    </Link>
+  </div>
+)
 
 export default Error404
