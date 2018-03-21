19268977ec8e1b1c1bb1b3b3a3c96d2b53cc5328
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2018-03-21 23:31:03 +0100

    image url base

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 // @flow
 export const API_URL = 'https://api.themoviedb.org/3'
 export const API_KEY = 'c5e4a1733c2995102fafe209c014e4c0'
+export const GET_IMAGE_URL_BASE = 'https://image.tmdb.org/t/p/'
